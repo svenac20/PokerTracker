92e038d5f615d86b8aaed0bc16b3eb493d23c851
Sven Scekic <svenscekic@microsoft.com>
2025-02-09 14:41:43 +0100

    Add env variables to azure static web app

--- poker-tracker-client-next/prisma/seed.ts
+++ poker-tracker-client-next/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -6,29 +6,143 @@ async function main() {
   // Add your seed data here
   await prisma.role.upsert({
     where: {
-        id: 1
+      id: 1,
     },
     update: {
-        name: "Admin"
+      name: "Admin",
     },
     create: {
-        id: 1,
-        name: "Admin",
-    }
+      id: 1,
+      name: "Admin",
+    },
   });
 
   await prisma.role.upsert({
     where: {
-        id: 2
+      id: 2,
     },
     update: {
-        name: "Player"
+      name: "Player",
     },
     create: {
-        id: 2,
-        name: "Player",
-    }
+      id: 2,
+      name: "Player",
+    },
   });
+
+  await prisma.country.upsert({
+    where: {
+      id: 1,
+    },
+    update: {
+      name: "Croatia",
+    },
+    create: {
+      id: 1,
+      name: "Croatia",
+    },
+  });
+
+  await prisma.gameType.upsert({
+    where: {
+      id: 1,
+    },
+    update: {
+      name: "PLO",
+    },
+    create: {
+      id: 1,
+      name: "PLO",
+    },
+  });
+
+  await prisma.gameType.upsert({
+    where: {
+      id: 2,
+    },
+    update: {
+      name: "NLO",
+    },
+    create: {
+      id: 2,
+      name: "NLO",
+    },
+  });
+
+  await prisma.town.upsert({
+    where: {
+      id: 1,
+    },
+    update: {
+      name: "Zagreb",
+    },
+    create: {
+      id: 1,
+      name: "Zagreb",
+      countryId: 1,
+    },
+  });
+
+  await prisma.town.upsert({
+    where: {
+      id: 2,
+    },
+    update: {
+      name: "Rijeka",
+      countryId: 1,
+    },
+    create: {
+      id: 2,
+      name: "Rijeka",
+      countryId: 1,
+    },
+  });
+
+  await prisma.casino.upsert({
+    where: {
+      id: 1,
+    },
+    update: {
+      name: "Cezar",
+      townId: 1,
+    },
+    create: {
+      id: 1,
+      name: "Cezar",
+      townId: 1,
+    },
+  });
+
+  await prisma.casino.upsert({
+    where: {
+      id: 2,
+    },
+    update: {
+      name: "Admiral",
+      townId: 1,
+    },
+    create: {
+      id: 2,
+      name: "Admiral",
+      townId: 1,
+    },
+  });
+
+  await prisma.casino.upsert({
+    where: {
+      id: 3,
+    },
+    update: {
+      name: "Senator",
+      townId: 2,
+    },
+    create: {
+      id: 3,
+      name: "Senator",
+      townId: 2,
+    },
+  });
+
   // Add more seed data as needed
 }
 
