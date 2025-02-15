e289bfcda6c1cbf9919e95e124f780745d297a8e
Sven Scekic <svenscekic@microsoft.com>
2025-02-15 14:23:01 +0100

    change axios to use https

--- poker-tracker-client-next/src/lib/axios.ts
+++ poker-tracker-client-next/src/lib/axios.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const axiosClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`, 
   timeout: 30000, 
+  httpsAgent: true
 });
 
 export default axiosClient;
