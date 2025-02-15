fdab9524f343cb7baa7455c87801691b3c423b06
Sven Scekic <svenscekic@microsoft.com>
2025-02-15 14:34:35 +0100

    fix axios url

--- poker-tracker-client-next/src/lib/axios.ts
+++ poker-tracker-client-next/src/lib/axios.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const axiosClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`, 
   timeout: 30000, 
-  httpsAgent: true
 });
 
 export default axiosClient;
