9ffb49bbbd36433f37da5ae7d388f3d739fdf99b
Sven Scekic <svenscekic@microsoft.com>
2025-03-19 09:59:35 +0100

    fix image route

--- poker-tracker-client-next/src/app/layout.tsx
+++ poker-tracker-client-next/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import ogImage from "/opengraph-image.png";
+import ogImage from "./opengraph-image.png";
 import "./globals.css";
 
 const geistSans = Geist({
