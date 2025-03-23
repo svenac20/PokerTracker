934cc8bad76fd8437e15e90a40c447f23987d700
Sven Scekic <svenscekic@microsoft.com>
2025-03-23 21:10:17 +0100

    fix next config for remote images. Add storage account secret and name to deployment pipieline

--- poker-tracker-client-next/next.config.ts
+++ poker-tracker-client-next/next.config.ts
@@ -1,15 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output:"standalone",
-  distDir: 'build',
+  output: "standalone",
+  distDir: "build",
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
     dirs: ["src/app", "src/components/custom", "src/hooks", "src/lib"],
   },
   images: {
-    domains: ["localhost", "pokerradarsa.blob.core.windows.net", "poker-radar.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pokerradarsa.blob.core.windows.net",
+        port: "",
+        pathname: "/casinos-images/**",
+        search: "",
+      },
+    ],
   },
   redirects: async () => {
     return [
@@ -19,7 +27,7 @@ const nextConfig: NextConfig = {
         permanent: true,
       },
     ];
-  }
+  },
 };
 
 export default nextConfig;
