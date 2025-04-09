1175e601618be98553e4136ac90774efb324eb9f
Sven Scekic <svenscekic@microsoft.com>
2025-04-09 11:35:56 +0200

    remove unsued imports

--- poker-tracker-client-next/src/app/login/page.tsx
+++ poker-tracker-client-next/src/app/login/page.tsx
@@ -1,24 +1,4 @@
-import GoogleSignInButton from "@/components/custom/googleSigninButton";
-import { LoadingSpinner } from "@/components/custom/loading";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
-import { loginSchema } from "@/lib/zod-schema";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Metadata } from "next";
-import { signIn } from "next-auth/react";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 import LoginPageForm from "./loginPageForm";
 
 export async function generateMetadata(): Promise<Metadata> {
